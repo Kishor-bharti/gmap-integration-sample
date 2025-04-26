ec2d876306bb5e411a74579df38052c3e4b8d167
Monster-i <kishorbharti010@gmail.com>
2025-04-27 00:52:46 +0530

    updated Database

--- db.sql
+++ db.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE books;
+CREATE DATABASE maps;
 CREATE TABLE user_locations (
   id INT AUTO_INCREMENT PRIMARY KEY,
   latitude DECIMAL(10, 8),
   longitude DECIMAL(11, 8),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-SELECT * FROM user_locations;
-DROP TABLE user_locations;
+-- SELECT * FROM user_locations;
+-- DROP TABLE user_locations;
